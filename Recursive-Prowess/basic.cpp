3557913313e54ca4ccf11b18c69a1fedc2984c62
Aditya <adityanegi804@gmail.com>
2026-01-11 23:29:55 +0530

    3 patterns done

---
+++ Recursive-Prowess/basic.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+    
+    
+    
+// Problem 1 - Print 1 to N using recursion
+// void fun(int n){
+    
+//     //base condition
+//     if(n == 0) return;
+    
+//     fun(n-1);
+//     cout<<n<<" ";
+// }
+// int main(){
+    
+//     int n;//5
+//     cin>>n;
+    
+//     fun(n);//5
+//     return 0;
+// }
+
+//Problem 2 - Print n to 1 using recursion
+// void fun(int n){
+    
+//     //base condition
+//     if(n == 0) return;
+    
+//     cout<<n<<" ";
+//     fun(n-1);
+    
+// }
+// int main(){
+    
+//     int n;//5
+//     cin>>n;
+    
+//     fun(n);//5
+//     return 0;
+// }
+
+
+// Problem 3 - Height of a binary tree
+
+// NOTE - if height defintion is given as number of nodes in the longest path from root to leaf -> return 0 in base conditon
+
+        //   if height defintion is given as number of edges in the longest path from root to leaf -> return -1 in base conditon
+        
+        
+// int height(node* root){
+    
+//     if(root == NULL) return -1;
+    
+//     return max(height(root->left), height(root->right))+1;
+// }
+
+
+// Problem 4 - Sort an array
+
+
+
+
+
+
+
+
+
+
+
