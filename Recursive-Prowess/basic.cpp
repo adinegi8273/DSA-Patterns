3918525839c1759d8e0ef2920496bf672b5f006e
Aditya <adityanegi804@gmail.com>
2026-01-19 08:50:23 +0530

    merge sort done

--- Recursive-Prowess/basic.cpp
+++ Recursive-Prowess/basic.cpp
@@ -59,6 +59,69 @@ using namespace std;
 
 // Problem 4 - Sort an array
 
+// 1 - Using merge sort - 
+
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+
+// void merge(int low,int mid,int high,vector<int>& arr){
+    
+    
+//     vector<int> temp;
+//     int i = low,j = mid+1;
+    
+//     while(i <= mid && j <= high){
+        
+//         if(arr[i] <= arr[j]) temp.push_back(arr[i++]);
+//         else temp.push_back(arr[j++]);
+//     }
+    
+//     while(i <= mid){
+//         temp.push_back(arr[i++]);
+//     }
+    
+//     while(j <= high){
+//         temp.push_back(arr[j++]);
+//     }
+    
+//     for(int i=0;i<temp.size();i++){
+//         arr[low+i] = temp[i];
+//     }
+    
+// }
+
+// void mergeSort(int low,int high,vector<int>& arr){//merge sort uses binary recursion
+    
+//     if(low < high){//will return if low >= high -> represent single element
+        
+//         int mid = (low + high)/2;
+        
+//         mergeSort(low,mid,arr);
+//         mergeSort(mid+1,high,arr);
+//         merge(low,mid,high,arr);
+//     }
+// }
+// int main(){
+    
+//     int n;
+//     cin>>n;
+    
+//     vector<int> arr(n);
+    
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+    
+//     mergeSort(0,n-1,arr);
+    
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+    
+//     return 0;
+// }
 
 
 
