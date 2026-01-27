555e6f4ea33de221419ee6a35e59a438564020b0
Aditya <adityanegi804@gmail.com>
2026-01-27 13:13:50 +0530

    Solved yesterday 2 patterns saved

--- Recursive-Prowess/basic.cpp
+++ Recursive-Prowess/basic.cpp
@@ -41,6 +41,7 @@ using namespace std;
 //     return 0;
 // }
 
+// ---------------------------------------------------------------------------------------------------------------
 
 // Problem 3 - Height of a binary tree
 
@@ -56,6 +57,7 @@ using namespace std;
 //     return max(height(root->left), height(root->right))+1;
 // }
 
+// ------------------------------------------------------------------------------------------------------------------
 
 // Problem 4 - Sort an array
 
@@ -124,11 +126,188 @@ using namespace std;
 // }
 
 
+// 2. Quick sort
+
+// Normal Quick sort
+
+// Method 1 - TC - O(nlogn) time in bets and average but O(n^2) in worst case SC - O(logn)
+
+// int partition(int low,int high,vector<int>& a){
+
+//         //select a pivot index
+//         int pivot = a[high];
+
+//         int i = low;
+//         int j = low-1;
+
+//         while(i <= high){
+//                 if(a[i] < pivot){
+//                         swap(a[i],a[j+1]);
+//                         j++;
+//                 }
+//                 i++;
+//         }
+
+//         swap(a[high],a[j+1]);
+//         return j+1;
+// }
+
+// void quickSort(int low,int high,vector<int>& a){
+
+//         if(low >= high){
+//                 return;
+//         }
+
+//         int pivot_index = partition(low,high,a);
+//         quickSort(low,pivot_index-1,a);
+//         quickSort(pivot_index+1,high,a);
+// }
+
+// int main(){
+//         int n;
+//         cin>>n;
+
+//         vector<int> a(n);
+
+//         for(int i=0;i<n;i++){
+//                 cin>>a[i];
+//         }
+
+//         quickSort(0,n-1,a);
+
+//         for(int i=0;i<n;i++){
+//                 cout<<a[i]<<" ";
+//         }
+
+//         return 0;
+// }
 
 
+// ---------------------------------------------------------------------------------------------------------
+
+// Problem - Sort a stack
 
 
+// Given a stack of integers st[]. Sort the stack in ascending order (smallest element at the bottom and largest at the top).
+
+// Input: st[] = [1, 2, 3]
+// Output: [3, 2, 1]
+
+// Time complexity is O(nlogn) and space complexity is O(n)
+
+// class Solution {
+//   public:
+//     void sortStack(stack<int> &st) {
+        
+        
+//         vector<int> temp;
+        
+//         while(!st.empty()){
+//             temp.push_back(st.top());
+//             st.pop();
+//         }
+        
+//         sort(temp.begin(),temp.end());
+        
+//         for(int elem:temp){
+//             st.push(elem);
+//         }
+    
+//     }
+// };
 
 
+// ----------------------------------------------------------------------------------------------------------------
+
+// Problem - Delete middle element from the stack using Recursion
+// Given a stack s, delete the middle element of the stack without using any additional data structure.
+
+// Middle element:- floor((size_of_stack+1)/2) (1-based indexing) from the bottom of the stack.
+
+// Note: The output shown by the compiler is the stack from top to bottom.
+
+// Examples:
+
+// Input: s = [10, 20, 30, 40, 50]
+// Output: [50, 40, 20, 10]
+// Explanation: The bottom-most element will be 10 and the top-most element will be 50. Middle element will be element at index 3 from bottom, which is 30. Deleting 30, stack will look like {10 20 40 50}.
+// Time complexity is O(n/2) and space is O(n/2)
+
+// class Solution {
+//   public:
+//     // Function to delete middle element of a stack.
+//     void deleteMid(stack<int>& s) {
+        
+//         int middle =floor((s.size()+1)/2);
+//         if(s.size() % 2 == 1){
+//             //the size  is odd
+//             middle = middle - 1;
+//         }
+//         stack<int> temp;
+//         while(middle--){
+//             temp.push(s.top());
+//             s.pop();
+//         }
+        
+//         if(s.size() == 0){
+//             //stack may have become empty
+//             return;
+//         }
+//         //now at teh top you have the middle element delete it now
+//         s.pop();
+        
+//         while(!temp.empty()){
+//             s.push(temp.top());
+//             temp.pop();
+//         }
+        
+//     }
+// };
+
+
+// ------------------------------------------------------------------------------------------------------------
+
+// Problem - Reverse a stack using Recursion
+
+// You are given a stack st[]. You have to reverse the stack.
+
+// class Solution {
+//   public:
+  
+//     void insertElementAtBottom(stack<int>& st,int& x){
+        
+//         if(st.empty()){
+//             st.push(x);
+//             return;
+//         }
+        
+//         int top = st.top();
+//         st.pop();
+//         insertElementAtBottom(st,x);
+//         st.push(top);
+//     }
+    
+//     void solve(stack<int>& st){
+        
+//         if(st.size() == 1){
+//             return;
+//         }
+        
+//         int top = st.top();
+//         st.pop();
+//         solve(st);
+        
+//         //when you backtrack whatever value u have at the top put that value at the bottom
+        
+//         insertElementAtBottom(st,top);
+        
+//     }
+//     void reverseStack(stack<int> &st) {
+//         // code here
+        
+        
+//         solve(st);
+//     }
+// };
 
 
